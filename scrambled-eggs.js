2cc1ccd6b762a8c0b383c40f016733a4c1072b0d
Bacon1153 <31748254+Bacon1153@users.noreply.github.com>
2019-03-12 23:40:54 -0400

    Basic framwork & megaminx

--- scrambled-eggs.js
+++ scrambled-eggs.js
@@ -1,9 +1,15 @@
 let scrambles = {};
-scrambles = require(`./scrambles/NNN/${NNN}`)
-// require the rest
+scrambles.clock = require("./scrambles/clock.js");
+scrambles.mega = require("./scrambles/mega.js");
+scrambles.pyra = require("./scrambles/pyra.js");
+scrambles.redi = require("./scrambles/redi.js");
+scrambles.skewb = require("./scrambles/skewb.js");
+scrambles.squan = require("./scrambles/squan.js");
 
 function Egg(cube, amount) {
-    
+	if(!amount || isNaN(amount)) amount = 1;
+//  using later for NNN cubes: scrambles = require(`./scrambles/NNN/${NNN}.js`);
+	return scrambles[cube].run(amount);
 }
 
 module.exports = Egg;
